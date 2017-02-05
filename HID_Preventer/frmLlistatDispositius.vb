3b3eb3a05d806a468aee070fc0b981a62da19fd0
Moises <moises_saus@hotmail.com>
2017-02-05 20:16:34 +0100

    Detecta els dispositius connectats

--- HID_Preventer/frmLlistatDispositius.vb
+++ HID_Preventer/frmLlistatDispositius.vb
@@ -1,23 +1,46 @@
 ﻿Imports System.Collections.Generic
+Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System
+Imports System.Management
+
 
 
 Public Class frmLlistatDispositius
     Private Sub frmLlistatDispositius_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim dispositius As New List(Of String)
-        dispositius.Add("teclat")
-        dispositius.Add("ratoli")
-        dispositius.Add("camara")
-        omplenaListBox(dispositius)
-
+        omplenaListBox(getConnectedDevices())
     End Sub
 
 
 
     Sub omplenaListBox(ByVal dispositius As List(Of String))
         For Each disp As String In dispositius
-            listDisp.Items.Add(disp)
+            If disp <> Nothing Then
+                listDisp.Items.Add(disp)
+            End If
         Next
     End Sub
+
+
+
+    Private Function getConnectedDevices() As List(Of String)
+        Dim info As System.Management.ManagementObject
+        Dim search As System.Management.ManagementObjectSearcher
+        Dim Name As String
+        Dim dispositius As New List(Of String)
+
+        search = New System.Management.ManagementObjectSearcher("SELECT * From Win32_PnPEntity")
+
+        For Each info In search.Get()
+            Name = CType(info("Caption"), String)
+            dispositius.Add(Name)
+        Next
+
+
+        Return dispositius
+    End Function
+
+
 
 
 End Class
