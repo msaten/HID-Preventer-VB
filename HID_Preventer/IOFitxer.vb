d8ee7d08c831e120fe08dedd64fe0cc01059e883
Moises <moises_saus@hotmail.com>
2017-02-05 19:05:52 +0100

    commit inicial

---
+++ HID_Preventer/IOFitxer.vb
@@ -0,0 +1,24 @@
+﻿Public Class IOFitxer
+    Private fitxer As String
+
+    Public Property getNomFitxer As String
+        Get
+            Return fitxer
+        End Get
+        Set(value As String)
+            fitxer = value
+        End Set
+    End Property
+
+    Sub llegirFitxer()
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText("C:\test.txt")
+        MsgBox(fileReader)
+    End Sub
+
+
+
+
+
+
+End Class
---
+++ HID_Preventer/frmLlistatDispositius.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmLlistatDispositius
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.listDisp = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'listDisp
+        '
+        Me.listDisp.FormattingEnabled = True
+        Me.listDisp.Location = New System.Drawing.Point(202, 255)
+        Me.listDisp.Name = "listDisp"
+        Me.listDisp.Size = New System.Drawing.Size(563, 251)
+        Me.listDisp.TabIndex = 0
+        '
+        'frmLlistatDispositius
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1045, 594)
+        Me.Controls.Add(Me.listDisp)
+        Me.Name = "frmLlistatDispositius"
+        Me.Text = "Dispositius"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents listDisp As ListBox
+End Class
