88dd9f7759a09d1f950546dbb2f28186c5344791
Moises <moises_saus@hotmail.com>
2017-02-05 19:42:11 +0100

    Afegir disp a ListBox

--- HID_Preventer/frmLlistatDispositius.vb
+++ HID_Preventer/frmLlistatDispositius.vb
@@ -1,5 +1,23 @@
-﻿Public Class frmLlistatDispositius
+﻿Imports System.Collections.Generic
+
+
+Public Class frmLlistatDispositius
     Private Sub frmLlistatDispositius_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim dispositius As New List(Of String)
+        dispositius.Add("teclat")
+        dispositius.Add("ratoli")
+        dispositius.Add("camara")
+        omplenaListBox(dispositius)
 
     End Sub
+
+
+
+    Sub omplenaListBox(ByVal dispositius As List(Of String))
+        For Each disp As String In dispositius
+            listDisp.Items.Add(disp)
+        Next
+    End Sub
+
+
 End Class
--- HID_Preventer/frmPrincipal.vb
+++ HID_Preventer/frmPrincipal.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Principal
-
+    Private Sub btnDispositius_Click(sender As Object, e As EventArgs) Handles btnDispositius.Click
+        Dim frmDispositius As New frmLlistatDispositius
+        frmDispositius.Show()
+    End Sub
 End Class
